0b0d7311dfd195fb9bc23d44ebacfff719f14c2d
sykim <sykim.finshot@gmail.com>
2019-04-30 17:38:55 +0900

    retrofit 관련 성공예제입니다 이것을 참고하세요 (브랜치는 이렇게 합니다)

--- app/src/main/java/com/coinshot/myfirebaseapp/service/FCMService.java
+++ app/src/main/java/com/coinshot/myfirebaseapp/service/FCMService.java
@@ -1,11 +1,10 @@
 package com.coinshot.myfirebaseapp.service;
 
+
 import com.coinshot.myfirebaseapp.model.Push;
 import com.coinshot.myfirebaseapp.model.Response;
 
 import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
@@ -15,11 +14,6 @@ public interface FCMService {
             "Content-Type: application/json",
             "Authorization: key=AAAA0qxo8nQ:APA91bEdUQrgLU-UiYUi7veXJFXPvKuj7VukwjjVUyEEPXnwJi4K2vC3-X92mlKrLAfZgo9a829waphnWFf16E081kI2GyMpt3-ksvPCPTGWTvfNDNYLXsE0JGQllmUITqafxGoQJ8iF"
     })
-    @POST("/")
+    @POST("/fcm/send") // https://fcm.googleapis.com/fcm/send
     Call<Response> postFCMBody(@Body Push push);
-
-    public static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://fcm.googleapis.com/fcm/send/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
 }
--- app/src/main/java/com/coinshot/myfirebaseapp/service/FirebaseInstanceIDService.java
+++ app/src/main/java/com/coinshot/myfirebaseapp/service/FirebaseInstanceIDService.java
@@ -10,8 +10,8 @@ import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.coinshot.myfirebaseapp.activity.MainActivity;
 import com.coinshot.myfirebaseapp.R;
+import com.coinshot.myfirebaseapp.activity.MainActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
